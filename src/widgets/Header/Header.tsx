79afa821f6ff3e0eddd1d710b61986d63bf764a1
Владислав Рыжов <vladryzov@gmail.cmo>
2025-05-04 16:23:01 +0300

    feat: Добавлена навигация в Header

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -3,16 +3,25 @@ import styles from './Header.module.css';
 import Logo from 'src/shared/ui/Logo/Logo';
 import ThemeSwitcher from 'src/widgets/ThemeSwitcher/ThemeSwitcher';
 import LanguageSwitcher from 'src/widgets/LanguageSwitcher/LanguageSwitcher';
+import { NavLink } from 'react-router-dom';
 
 const Header: FC = () => {
   return (
     <header className={styles.header}>
       <Logo />
+
+      <nav className={styles.nav}>
+        <NavLink to="/operations" className={({ isActive }) => (isActive ? styles.active : undefined)}>
+          Операции
+        </NavLink>
+        <NavLink to="/profile" className={({ isActive }) => (isActive ? styles.active : undefined)}>
+          Профиль
+        </NavLink>
+      </nav>
+
       <div className={styles.controls}>
-        <div className={styles.controls}>
-          <ThemeSwitcher />
-          <LanguageSwitcher />
-        </div>
+        <ThemeSwitcher />
+        <LanguageSwitcher />
       </div>
     </header>
   );
